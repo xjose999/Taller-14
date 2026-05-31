920de59214d840b520b704e707c76d070a957f8e
xjose999 <josejoestar80@gmail.com>
2026-05-31 13:51:20 -0500

    Ejercicio 1.4

--- PruebaPolimorfismoFiguras.java
+++ PruebaPolimorfismoFiguras.java
@@ -1,2 +1,17 @@
 public class PruebaPolimorfismoFiguras {
+    public static void main(String[] args) {
+
+        Figura f1 = new Circulo();
+        Figura f2 = new Rectangulo();
+
+
+        ((Circulo) f1).radio = 4.0;
+
+        ((Rectangulo) f2).base = 5.0;
+        ((Rectangulo) f2).altura = 3.0;
+
+
+        f1.calcularArea(); // Llama al método de Circulo
+        f2.calcularArea(); // Llama al método de Rectangulo
+    }
 }
